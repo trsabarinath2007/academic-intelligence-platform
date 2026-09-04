7ea163048f5dd620b871343083ba744bf41320ef
Sabarinath TR <trsabarinath2007@gmail.com>
2026-09-04 13:19:20 +0530

    done

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Academic Intelligence Platform</h1>
+      <p>Frontend setup is working successfully.</p>
+    </div>
+  );
+}
+
+export default App;
