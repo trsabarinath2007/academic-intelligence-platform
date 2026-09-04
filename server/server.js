f0ef0f5d9df2ad4d3a6ec6af16ead3600f95dbb7
Sabarinath TR <trsabarinath2007@gmail.com>
2026-09-04 16:28:52 +0530

    c

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
