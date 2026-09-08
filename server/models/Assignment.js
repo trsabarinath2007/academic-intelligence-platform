a23038293680b541e73e9301c807c5534e8e5dee
Sabarinath TR <trsabarinath2007@gmail.com>
2026-09-08 10:44:52 +0530

    ddf

---
+++ server/models/Assignment.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const assignmentSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+
+    totalMarks: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Assignment", assignmentSchema);
