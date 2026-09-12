3c25f710870df02b7bea998b6c0973f71b52bae4
Sabarinath TR <trsabarinath2007@gmail.com>
2026-09-12 20:46:20 +0530

    koo

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,11 +3,29 @@ const express = require("express");
 const {
   registerUser,
   loginUser,
+  resetUserPassword,
 } = require("../controllers/authController");
+
+const {
+  protect,
+  authorize,
+} = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+// Register User
 router.post("/register", registerUser);
+
+// Login User
 router.post("/login", loginUser);
+
+// Reset User Password
+// Only admin and faculty can reset passwords
+router.put(
+  "/reset-password",
+  protect,
+  authorize("admin", "faculty"),
+  resetUserPassword
+);
 
 module.exports = router;
