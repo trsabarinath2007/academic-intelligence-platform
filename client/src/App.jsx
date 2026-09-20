54ae3c33cee3e7be949e38b8fd6e921237bee342
Sabarinath TR <trsabarinath2007@gmail.com>
2026-09-20 17:09:47 +0530

    lpp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import StudentDashboard from "./pages/StudentDashboard";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Home />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route
+          path="/student-dashboard"
+          element={<StudentDashboard />}
+        />
+
         <Route path="*" element={<NotFound />} />
+
       </Routes>
     </BrowserRouter>
   );
