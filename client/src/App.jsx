5ac6523da2237c5ff8d135683dd1c181743a086b
Sabarinath TR <trsabarinath2007@gmail.com>
2026-09-22 09:10:26 +0530

    veafea

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import NotFound from "./pages/NotFound";
 import StudentProfile from "./pages/StudentProfile";
 import Courses from "./pages/Courses";
 import AcademicRecords from "./pages/AcademicRecords";
+import Attendance from "./pages/Attendance";
+
 
 function App() {
   return (
